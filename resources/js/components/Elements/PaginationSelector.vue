d743cd5183aea9229c0b016e04f63ca947848c26
Donny <qq1507438135@163.com>
2024-07-22 14:30:28 +0800

    update paginate

---
+++ resources/js/components/Elements/PaginationSelector.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import range from 'lodash/range'
+import Select from '@/components/Elements/Select.vue'
+import { onBeforeMount } from 'vue'
+
+interface Props {
+  perPage: number
+  perPageOptions: number[]
+  setPerPage: (perPage: number) => void
+}
+
+withDefaults(defineProps<Props>(), {
+  perPage: 15
+})
+</script>
+
+<template>
+  <Select :current="perPage" :on-click="setPerPage" :options="perPageOptions" />
+</template>
